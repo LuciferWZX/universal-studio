58f195d8228cd199fe5af3a647953cf2bf03b528
zhixin.wu <zhixin.wu@cyclone-robotics.com>
2022-05-07 13:12:41 +0800

    update getType

--- packages/support/lib/helpUtils.js
+++ packages/support/lib/helpUtils.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getType = void 0;
 var getType = function (a) {
     if (Object.prototype.toString.call(a).includes("[object ")) {
-        return Object.prototype.toString.call(a).slice(7, -2);
+        return Object.prototype.toString.call(a).slice(7, -1);
     }
+    console.log(123, Object.prototype.toString.call(a));
     return "unknow type";
 };
 exports.getType = getType;
